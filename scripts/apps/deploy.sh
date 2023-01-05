20054ea36a9039c2ae6a76a29bf2e780b538c1c4
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2023-01-05 10:59:11 -0600

    +prod deploy

--- scripts/apps/deploy.sh
+++ scripts/apps/deploy.sh
@@ -3,6 +3,11 @@ if [ $1 == "all" ]
 then
     kubectl apply -f ../../k8s/tracer/deployment.yaml
     kubectl apply -f ../../k8s/consumer/deployment.yaml
+    kubectl apply -f ../../k8s/producer/configmap.yaml
+    kubectl apply -f ../../k8s/producer/deployment.yaml
+elif [ $1 == "producer" ]
+then
+    kubectl apply -f ../../k8s/producer/configmap.yaml
     kubectl apply -f ../../k8s/producer/deployment.yaml
 else
     kubectl apply -f ../../k8s/$1/deployment.yaml
