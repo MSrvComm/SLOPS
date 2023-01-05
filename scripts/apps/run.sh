3ad02fd597482bb3b029f05c8f681f0557c56fde
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2023-01-05 17:13:20 -0600

    +bugfix

--- scripts/apps/run.sh
+++ scripts/apps/run.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
+BASEDIR=$(pwd)
+if [[ $BASEDIR == *"apps" ]]
+then
+    BASEDIR+="/../.."
+elif [[ $BASEDIR == *"scripts" ]]
+then
+    BASEDIR+="/.."
+fi
+
 rate=${rate:-200}
 iter=${iter:-1000}
 keys=${keys:-2800}
-burst=${rate}
+burst=$rate
+
+while [ $# -gt 0 ]; do
+    if [[ $1 == "--"* ]]; then
+        v="${1/--/}"
+        declare "$v"="$2"
+        shift   
+    fi
+    shift
+done
 
 PORT=$(kubectl get svc producer -n slops -o go-template='{{range.spec.ports}}{{if .nodePort}}{{.nodePort}}{{"\n"}}{{end}}{{end}}')
-../../SLOPSClient/SLOPSClient --rate ${rate} --iter ${iter} --burst ${burst} --keys ${keys} --url http://localhost:$PORT/new
+$BASEDIR/SLOPSClient/SLOPSClient -rate $rate -iter $iter -burst $burst -keys $keys -url http://localhost:$PORT/new
