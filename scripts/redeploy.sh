f03156a7a5b64f43bb603ccc1baaeea3449ecac3
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2023-01-11 16:42:12 -0600

    +bugfx

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -3,5 +3,5 @@
 kubectl wait --for=delete deployment/consumer -n slops
 kubectl wait --for=delete deployment/producer -n slops
 ./scripts/kafka/delete.sh
-./scripts/kafka/create.sh $1
+./scripts/kafka/create.sh 10
 ./scripts/apps/deploy.sh all
