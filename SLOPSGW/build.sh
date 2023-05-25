377453906bf23f03c6e56788a8fb7de5cf5b8f4d
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2023-05-24 19:08:21 -0500

    +Gateway

---
+++ SLOPSGW/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker build -t ratnadeepb/slops-gateway:latest .
+docker push ratnadeepb/slops-gateway:latest
--- scripts/apps/deploy.sh
+++ scripts/apps/deploy.sh
@@ -10,6 +10,7 @@ fi
 
 if [[ $1 == "all" ]]
 then
+    kubectl apply -f $BASEDIR/k8s/gateway/deployment.yaml
     kubectl apply -f $BASEDIR/k8s/consumer/deployment.yaml
     kubectl apply -f $BASEDIR/k8s/producer/configmap.yaml
     kubectl apply -f $BASEDIR/k8s/producer/deployment.yaml
@@ -17,6 +18,9 @@ elif [[ $1 == "producer" ]]
 then
     kubectl apply -f $BASEDIR/k8s/producer/configmap.yaml
     kubectl apply -f $BASEDIR/k8s/producer/deployment.yaml
+elif [[ $1 == "gateway" ]]
+then
+    kubectl apply -f $BASEDIR/k8s/gateway/deployment.yaml
 else
     kubectl apply -f $BASEDIR/k8s/$1/deployment.yaml
 fi
