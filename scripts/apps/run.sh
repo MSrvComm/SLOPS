ca6b801a7ee621cbe425e00f12f363708becaf96
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2023-05-26 06:45:06 -0500

    +run through GW

--- scripts/apps/run.sh
+++ scripts/apps/run.sh
@@ -8,7 +8,7 @@ then
     BASEDIR+="/.."
 fi
 
-gw=${gw:-0}
+# gw=${gw:-0}
 rate=${rate:-1000}
 iter=${iter:-300000} # 5 minutes
 keys=${keys:-28000000}
@@ -22,11 +22,16 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-if [ $gw == 0 ]
-then
-    PORT=$(kubectl get svc gateway -n slops -o go-template='{{range.spec.ports}}{{if .nodePort}}{{.nodePort}}{{"\n"}}{{end}}{{end}}')
-else
-    PORT=$(kubectl get svc producer -n slops -o go-template='{{range.spec.ports}}{{if .nodePort}}{{.nodePort}}{{"\n"}}{{end}}{{end}}')
-fi
+PORT=$(kubectl get svc gateway -n slops -o go-template='{{range.spec.ports}}{{if .nodePort}}{{.nodePort}}{{"\n"}}{{end}}{{end}}')
+$BASEDIR/SLOPSClient/SLOPSClient -rate $rate -iter $iter -keys $keys -url http://localhost:$PORT/new
 
-    $BASEDIR/SLOPSClient/SLOPSClient -rate $rate -iter $iter -keys $keys -url http://localhost:$PORT/new
+# if [ $gw == 0 ]
+# then
+#     rate=2000
+#     iter=600000
+#     PORT=$(kubectl get svc gateway -n slops -o go-template='{{range.spec.ports}}{{if .nodePort}}{{.nodePort}}{{"\n"}}{{end}}{{end}}')
+#     $BASEDIR/SLOPSClient/SLOPSClient -rate $rate -iter $iter -keys $keys -url http://localhost:$PORT/new
+# else
+#     PORT=$(kubectl get svc producer -n slops -o go-template='{{range.spec.ports}}{{if .nodePort}}{{.nodePort}}{{"\n"}}{{end}}{{end}}')
+#     $BASEDIR/SLOPSClient/SLOPSClient -rate $rate -iter $iter -keys $keys -url http://localhost:$PORT/new
+# fi
