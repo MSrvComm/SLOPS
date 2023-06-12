ed1590cf0766c3cbee7653bda63de3942bd022ed
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2023-06-12 06:33:33 -0500

    +tidy in build scripts

--- SLOPSController/build.sh
+++ SLOPSController/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+go mod tidy
 docker build -t ratnadeepb/slops-controller:latest .
 docker push ratnadeepb/slops-controller:latest
--- SLOPSGW/build.sh
+++ SLOPSGW/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+go mod tidy
 docker build -t ratnadeepb/slops-gateway:latest .
 docker push ratnadeepb/slops-gateway:latest
