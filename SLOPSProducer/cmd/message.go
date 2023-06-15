300bcbbb12d402838d1befb6ac8015b9079f2014
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2023-06-15 05:34:10 -0500

    debug print

--- SLOPSProducer/cmd/message.go
+++ SLOPSProducer/cmd/message.go
@@ -29,28 +29,29 @@ func (app *Application) NewMessage(c *gin.Context) {
 	log.Println("message sending")
 	// Use the basic version.
 	if app.vanilla {
-		log.Println("Basic Kafka")
 		partition, err := hash(input.Key, app.conf.Partitions)
+		app.logger.Printf("Kafka: Hashing new key to partition %d of %d partitions\n.", partition, app.conf.Partitions)
 		if err != nil {
 			log.Printf("Failed to hash key %s with error %v\n", input.Key, err)
 		}
 		go app.Produce(input.Key, input.Body, partition)
 	} else { // Use the SLOPS algorithm.
-		log.Println("SMALOPS")
 		var partition int32
 		if rec := app.partitionMap.GetKey(input.Key); rec == nil { // Use KeyMap to decide partition.
 			partition, err = hash(input.Key, app.conf.Partitions)
 			if err != nil {
 				log.Printf("Failed to hash key %s with error %v\n", input.Key, err)
 			}
+			app.logger.Printf("SMALOPS: Hashing new key to partition %d of %d partitions\n.", partition, app.conf.Partitions)
 		} else {
+			app.logger.Printf("SMALOPS: Sending to partition %d of %d partitions\n.", partition, app.conf.Partitions)
 			partition = int32(rec.Partition)
 			// Message Set header will be added by `Producer` when message is sent.
 		}
 		go app.Produce(input.Key, input.Body, partition)
 	}
 
-	log.Println("Received new request:", input)
+	app.logger.Println("Received new request:", input)
 }
 
 func hash(key string, numPartitions int32) (int32, error) {
