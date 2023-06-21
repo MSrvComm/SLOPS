a527eb484dc84b24b1160f9f79ae43bc729871e7
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2023-06-21 18:22:34 -0500

    +partitions

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -3,5 +3,5 @@
 kubectl wait --for=delete deployment/consumer -n slops
 kubectl wait --for=delete deployment/producer -n slops
 ./scripts/kafka/delete.sh
-./scripts/kafka/create.sh 10
+./scripts/kafka/create.sh 100
 ./scripts/apps/deploy.sh all
