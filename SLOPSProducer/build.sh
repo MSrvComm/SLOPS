61eed1878353a695f824df5c4fd287e3964c50e9
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2023-07-01 10:16:42 -0500

    update names

--- SLOPSProducer/build.sh
+++ SLOPSProducer/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 go mod tidy
-docker build -t ratnadeepb/slops-producer-state:latest .
-docker push ratnadeepb/slops-producer-state:latest
+docker build -t ratnadeepb/slops-producer-state-tol:latest .
+docker push ratnadeepb/slops-producer-state-tol:latest
